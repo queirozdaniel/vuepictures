cd2cde7b122fa4052a0e3de50268cc29244cd59e
queirozdaniel <danielqueirozak77@gmail.com>
2020-09-01 15:19:56 -0300

    Adicionando filtro

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,51 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto of fotos"><img :src="foto.url" :alt="foto.titulo" /></li>
+  <div class="corpo">
+    <h1 class="texto-center">{{ titulo }}</h1>
+    <input
+      type="search"
+      v-on:input="filtro = $event.target.value"
+      class="filtro"
+      placeholder="filtre por parte do título"
+    />
+    {{ filtro }}
+    <ul class="lista_fotos">
+      <li
+        class="lista_fotos_item"
+        v-for="(foto, index) of fotosComFiltro"
+        :key="index"
+      >
+        <painel :titulo="foto.titulo">
+          <img class="img-responsiva" :src="foto.url" :alt="foto.titulo" />
+        </painel>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from "./components/shared/painel/Painel.vue";
+
 export default {
+  components: {
+    Painel
+  },
   data() {
     return {
       titulo: "Vue Pictures",
-      fotos: []
+      fotos: [],
+      filtro: ""
     };
+  },
+  computed: {
+    fotosComFiltro() {
+      if (this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), "i");
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+        return this.fotos;
+      } else {
+        return this.fotos;
+      }
+    }
   },
   created() {
     let promise = this.$http.get("http://localhost:3000/v1/fotos");
@@ -27,4 +59,27 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.corpo {
+  font-family: "Helvetica", sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+
+.texto-center {
+  text-align: center;
+}
+
+.lista_fotos {
+  list-style-type: none;
+}
+
+.lista_fotos .lista_fotos_item {
+  display: inline-block;
+}
+
+.filtro {
+  display: block;
+  width: 100%;
+}
+</style>
